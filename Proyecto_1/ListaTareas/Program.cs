5fac2072cd38acbb1387f73e7cc6fafa8ea5b516
AlexAP-pixel <alejandroaguilar98787@outlook.com>
2025-06-30 19:24:12 -0600

    Primeros proyectos

---
+++ Proyecto_1/ListaTareas/Program.cs
@@ -0,0 +1,96 @@
+﻿using System; // Nombre del archivo: Program.cs
+using System.Collections.Generic;
+
+class Program
+{
+    static List<string> tareas = new List<string>(); // Lusta tareas
+    
+    static void Main(string[] strings)
+    {
+        while (true)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("Lista de Tareas");
+            Console.WriteLine("---------------");
+            Console.WriteLine("1. Ver tarea");
+            Console.WriteLine("2. Agregar tarea");
+            COnsole.WriteLine("3. Eliminar tarea");
+            Console.WriteLine("4. Salir");
+            Console.Write("\nSeleccione una opción: ");
+
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    VerTareas();
+                    break;
+                case "2":
+                    AgregarTarea();
+                    break;
+                case "3":
+                    EliminarTarea();
+                    break;
+                case "4":
+                    return;
+                default:
+                    Console.WriteLine("Opción no válida. Presione cualquier tecla para continuar...");
+                    break;
+            }
+        }
+    }
+
+    static void VerTareas()
+    {
+        if (tareas.Count == 0)
+        {
+            Console.WriteLine("No hay tareas en la lista.");
+        }
+        else
+        {
+            Console.WriteLine("Tareas:");
+            for (int i = 0; i < tareas.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {tareas[i]}");
+            }
+        }
+    }
+
+    static void AgregarTarea()
+    {
+        Console.Write("Ingresa la nueva tarea: ");
+        string tarea = Console.ReadLine();
+
+        if (!string.IsNullOrWhiteSpace(tarea))
+        {
+            tareas.Add(tarea);
+            Console.WriteLine("Tarea agregada con éxito!");
+        }
+        else
+        {
+            Console.WriteLine("La tarea no puede estar vacía");
+        }
+    }
+
+    static void EliminarTarea()
+    {
+        VerTareas();
+        if (tareas.Count > 0)
+        {
+            Console.Write("Ingresa el número de la tarea a eliminar: ");
+            if (int.TryParse(Console.ReadLine(), out int indice) && indice > 0 && indice <= tareas.Count)
+            {
+                tareas.RemoveAt(indice - 1);
+                Console.WriteLine("Tarea eliminada con éxito!");
+            }
+            else
+            {
+                Console.WriteLine("Número de tarea no válido.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No hay tareas para eliminar.");
+        }
+    }
+}
---
+++ app/Program.cs
@@ -0,0 +1,49 @@
+﻿//Console.WriteLine("Hello, World!");
+/*
+Variables:
+
+string libro = "C# Programming";
+int numero = 42;
+float decimalNumber = 3.14f;
+bool isTrue = true;
+bool isFalse = false;
+
+*/
+
+// Lista de numeros
+int[] numeros = { 1, 2, 3, 4, 5 };
+Console.WriteLine(numeros[0]);
+
+// Listas dinamicas
+List<int> listaNumeros = new List<int> { 1, 2, 3, 4, 5 };
+Console.WriteLine(listaNumeros[0]);
+
+Console.WriteLine(string.Join(",", listaNumeros));
+
+// Listas de texto
+List<string> listaTextos = new List<string> { "C#", "Java", "Python" };
+Console.WriteLine(string.Join(",", listaTextos));
+
+// Listas de datos dinamicos
+List<dynamic> listaDinamica = new List<dynamic> { "C#", 42, 3.14f, true };
+Console.WriteLine(string.Join(",", listaDinamica));
+
+// Diccionario
+Dictionary<string, int> diccionario = new Dictionary<string, int>
+{
+    { "C#", 1 },
+    { "Java", 2 },
+    { "Python", 3 }
+};
+
+diccionario.Add("JavaScript", 4);
+Console.WriteLine("Diccionario:");
+Console.WriteLine(diccionario["C#"]);
+
+// constantes
+const string CONSTANTE = "Valor constante";
+
+// Operadores
+int a = 10;
+int b = 5;
+Console.WriteLine($"Suma: {a + b}");
